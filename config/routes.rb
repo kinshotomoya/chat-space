94757197e5feac007e18986037766c0e595e30f8
金正朋也 <mhtnim1023@gmail.com>
2016-12-10 01:46:42 +0900

    Change line

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'groups#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
   resources :groups
 end
