650da2ffa107b73b827a1dbb62bf481e85068e78
金正朋也 <mhtnim1023@gmail.com>
2016-12-11 21:38:19 +0900

    Delete devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers =>{
-    :registrations =>'users/registrations',
-    :sessions =>'users/sessions'
-  }
+  devise_for :users
   root 'groups#index'
   resources :groups
 end
