b06d777c640f207ec273d5070aa2075a419f8f72
金正朋也 <mhtnim1023@gmail.com>
2016-12-11 22:33:04 +0900

    Add devise form_for to update user informations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+    put 'user' => 'devise/registrations#update', as: :update_user_registration
   root 'groups#index'
   resources :groups
 end
