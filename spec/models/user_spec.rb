2ebbff91d114299979b56fba34dcb03b14ce5fc2
金正朋也 <mhtnim1023@gmail.com>
2016-12-15 00:22:17 +0900

    Do write a test code on the user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+
+    it "is valid with a name, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+end
