f31b5bebbd6d8bfc919f57de098cb403e21d8cf3
金正朋也 <mhtnim1023@gmail.com>
2017-01-15 13:13:42 +0900

    delete editting function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,6 @@
 class GroupsController < ApplicationController
 
   def index
-    @group = Group.find(1)
   end
 
   def new
@@ -10,16 +9,6 @@ class GroupsController < ApplicationController
   def create
     Group.create(name: params[:chat_group][:name])
     redirect_to action: :index
-  end
-
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-    group = Group.find(params[:id])
-    group.update(name: params[:group][:name])
-    redirect_to action: :index, :notice => "更新完了"
   end
 
 
