4296ee61dfd09c6051437baffd5ef717336175cb
金正朋也 <mhtnim1023@gmail.com>
2017-01-22 14:34:01 +0900

    Edit chats_controllers in order to save messages in the DB

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,5 +2,25 @@ class ChatsController < ApplicationController
 
   def index
      @user_groups = UserGroup.where(user_id: current_user.id)
+     @chat = Chat.new
+     @group = Group.find(params[:group_id])
+
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to group_chats_path(@chat.group_id)
+    else
+
+    end
+
+  end
+
+
+  private
+  def chat_params
+    params.require(:chat).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
+
