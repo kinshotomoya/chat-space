94f9b46db6cec6fdeb4dfb2ae09285f6340d8012
金正朋也 <mhtnim1023@gmail.com>
2017-02-16 23:06:26 +0900

    Edit chat.rb rsepc

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :chat do
-    text           {Faker::Lorem.paragraph}
+    text           {Faker::Lorem.sentence}
     user_id        {Faker::Number.number(1)}
     group_id       {Faker::Number.number(2)}
     image          {Faker::Avatar.image}
--- spec/factories/user_groups.rb
+++ spec/factories/user_groups.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :user_group do
-    user_id            {Faker::Number.number(2)}
+    user_id            {Faker::Number.number(1)}
     group_id           {Faker::Number.number(2)}
   end
 end
