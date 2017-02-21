d96ff857579f52b40cfbd55bb54d72cfe306c1a8
金正朋也 <mhtnim1023@gmail.com>
2017-02-21 20:51:14 +0900

    Edit chats_controller for Javascript

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,22 +5,28 @@ class ChatsController < ApplicationController
      @groups = current_user.groups
      @users = @group.users
      @chat = Chat.new
+     @chats = @group.chats
 
   end
 
   def create
     @chat = Chat.new(chat_params)
-    if @chat.save
-      redirect_to group_chats_path(@chat.group_id)
-    else
-      flash[:alert] = "メッセージを入力してください。"
-    end
+      if @chat.save
+        respond_to do |format|
+          format.html{redirect_to group_chats_path(@chat.group_id)}
+          format.json{
+            render json: {text: @chat.text, name: current_user.name, time: @chat.created_at}
+          } #json形式でこの値を送ることができる。次はchat.jsを見る
+        end
+      else
+        redirect_to group_chats_path(@chat.group_id), alert: "メッセージを入力してください。"
+      end
   end
 
 
   private
   def chat_params
-    params.require(:chat).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
 
